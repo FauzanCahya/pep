b289bf0615d8cdff143074dd6a61def9de11bd6a
MSI\IRWAN <fauzancahya16@gmail.com>
2018-03-20 10:36:58 +0700

    no message

---
+++ application/models/peminjaman_barang_m.php
@@ -0,0 +1,151 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Peminjaman_barang_m extends CI_Model
+{
+	function __construct() {
+		  parent::__construct();
+		  $this->load->database();
+	}
+
+	function simpan_data_barang($no_spb,$tanggal,$uraian,$nama,$departemen)
+	{
+		$sql = "
+			INSERT INTO tb_peminjaman_barang (
+				no_spb,
+				tanggal,
+				uraian,
+				user,
+				status,
+				divisi
+			) VALUES (
+				'$no_spb',
+				'$tanggal',
+				'$uraian',
+				'$nama',
+				'0',
+				'$departemen'
+			)";
+		$this->db->query($sql);
+	}
+
+	function simpan_data_barang_detail($id_peminjaman_baru,$id_produk, $nama_produk,$keterangan,$kuantitas,$satuan,$harga,$jumlah)
+	{
+		$kuantitas 	= str_replace(',', '', $kuantitas);
+		$harga 		= str_replace(',', '', $harga);
+		$jumlah 	= str_replace(',', '', $jumlah);
+
+		$sql = "
+			INSERT INTO tb_peminjaman_barang_detail (
+				id_induk,
+				id_produk,
+				nama_produk,
+				keterangan,
+				kuantitas,
+				satuan,
+				harga,
+				jumlah,
+				status_pinjam,
+				sisa_jumlah
+			) VALUES (
+				'$id_peminjaman_baru',
+				'$id_produk',
+				'$nama_produk',
+				'$keterangan',
+				'$kuantitas',
+				'$satuan',
+				'$harga',
+				'$jumlah',
+				'0',
+				'$kuantitas'
+			)";
+		$this->db->query($sql);
+	}
+
+	function lihat_data_peminjaman()
+	{
+		$sql = "
+			SELECT * FROM tb_peminjaman_barang ";
+
+		return $this->db->query($sql)->result();
+	}
+
+	function lihat_data_barang()
+	{
+		$sql = "
+			SELECT * FROM master_barang ";
+
+		return $this->db->query($sql)->result();
+	}
+
+	function hapus_peminjaman($id)
+	{
+		$sql = "DELETE FROM  tb_peminjaman_barang WHERE id_peminjaman = '$id' " ;
+		$this->db->query($sql);
+
+		$sql2 = "DELETE FROM  tb_peminjaman_barang_detail WHERE id_induk = '$id' " ;
+		$this->db->query($sql2);
+	}
+
+	function data_peminjaman_id($id)
+	{
+		$sql = "SELECT * FROM tb_peminjaman_barang WHERE id_peminjaman = '$id' ";
+		$query = $this->db->query($sql);
+		return $query->row();
+	}
+
+	function data_peminjaman_detail_id($id)
+	{
+		$sql = "SELECT * FROM tb_peminjaman_barang_detail WHERE id_induk = '$id' ";
+		$query = $this->db->query($sql);
+		return $query->result();
+	}
+
+	function ubah_data_peminjaman($id,$no_spb,$tanggal,$uraian)
+	{
+		$sql = "
+			UPDATE tb_peminjaman_barang SET 
+				no_spb  	= '$no_spb',
+				tanggal 	= '$tanggal',
+				uraian  	= '$uraian'
+			WHERE id_peminjaman  = '$id'
+		";
+		$this->db->query($sql);
+	}
+
+	function save_next_nomor($tipe)
+	{
+		$sql = "
+			UPDATE ak_nomor SET 
+				NEXT_NOMOR  	= NEXT_NOMOR + 1
+			WHERE TIPE  = '$tipe'
+		";
+		$this->db->query($sql);
+	}
+
+	function ubah_data_peminjaman_detail($id,$nama_produk,$keterangan,$kuantitas,$satuan,$harga,$jumlah)
+	{
+		$kuantitas 	= str_replace(',', '', $kuantitas);
+		$harga 		= str_replace(',', '', $harga);
+		$jumlah 	= str_replace(',', '', $jumlah);
+		
+		$sql = "
+			UPDATE tb_peminjaman_barang_detail SET 
+				nama_produk = '$nama_produk',
+				keterangan  = '$keterangan',
+				kuantitas  	= '$kuantitas',
+				satuan  	= '$satuan',
+				harga  		= '$harga',
+				jumlah  	= '$jumlah'
+			WHERE id_induk  = '$id'
+		";
+		$this->db->query($sql);
+	}
+
+	function get_produk_detail($id_barang){
+        $sql = "
+        SELECT * FROM master_barang WHERE id_barang = $id_barang
+        ";
+
+        return $this->db->query($sql)->row();
+    }
+}
