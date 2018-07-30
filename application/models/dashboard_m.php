62c72c4fd5212e6a7518bfc194cb2caac388d396
Support-PC\Support <vantdephen@gmail.com>
2018-07-30 11:49:16 +0700

    a

--- application/models/dashboard_m.php
+++ application/models/dashboard_m.php
@@ -41,46 +41,6 @@ class Dashboard_m extends CI_Model
 		$query = $this->db->query($sql);
 		return $query->row();
 	}
-
-	function get_menu_1($id_pegawai){
-		$sql = "
-		SELECT a.* FROM kepeg_menu_1 a 
-		JOIN (
-			SELECT ID_MENU FROM kepeg_hak_akses
-			WHERE ID_PEGAWAI = '$id_pegawai' AND KET = 'MENU_PORTAL'
-		) b ON a.ID = b.ID_MENU
-        ORDER BY a.URUT ASC
-		";
-
-		return $this->db->query($sql)->result();
-	}
-
-	function get_menu_2($id_pegawai, $id_menu1){
-		$sql = "
-		SELECT a.* FROM kepeg_menu_2 a 
-		JOIN (
-			SELECT ID_MENU FROM kepeg_hak_akses
-			WHERE ID_PEGAWAI = '$id_pegawai' AND KET = 'MENU_2'
-		) b ON a.ID = b.ID_MENU
-		WHERE a.ID_MENU_1 = '$id_menu1'
-        ORDER BY a.URUT ASC
-		";
-
-		return $this->db->query($sql)->result();
-	}
-
-	function get_menu_3($id_pegawai, $id_menu2){
-		$sql = "
-		SELECT a.* FROM kepeg_menu_3 a 
-		JOIN (
-			SELECT ID_MENU FROM kepeg_hak_akses
-			WHERE ID_PEGAWAI = '$id_pegawai' AND KET = 'MENU_3'
-		) b ON a.ID = b.ID_MENU
-		WHERE a.ID_MENU_2 = '$id_menu2'
-        ORDER BY a.URUT ASC
-		";
-
-		return $this->db->query($sql)->result();
-	}
+	
 
 }
--- application/views/manajemen_user_v.php
+++ application/views/manajemen_user_v.php
@@ -1,6 +1,8 @@
 <script src="<?php echo base_url(); ?>js/jquery-1.11.1.min.js" type="text/javascript"></script>
 <style type="text/css">
-#view_hak_akses{
+#view_hak_akses,
+#view_tambah,
+#view_ubah{
 	display: none;
 }
 </style>
@@ -9,6 +11,16 @@ $(document).ready(function(){
 	<?php if($this->session->flashdata('simpan')){ ?>
 	berhasil();
 	<?php } ?>
+	
+	$('#btn_tambah').click(function(){
+		$('#view_data').hide();
+		$('#view_tambah').show();
+	});
+
+	$('#btn_batal').click(function(){
+		$('#view_data').show();
+		$('#view_tambah').hide();
+	});
 
 	$('#btn_batal_ha').click(function(){
 		$('#view_hak_akses').hide();
@@ -16,109 +28,74 @@ $(document).ready(function(){
 	});
 });
 
-function hapus_kategori(id){
-	$('#popup_hapus').css('display','block');
-	$('#popup_hapus').show();
+function ubah_data(id){
+	$('#view_ubah').show();
+	$('#view_data').hide();
 
-		$.ajax({
-		url : '<?php echo base_url(); ?>kategori_barang_c/data_kategori_id',
+	$.ajax({
+		url : '<?php echo base_url(); ?>manajemen_user_c/get_user_id',
 		data : {id:id},
 		type : "POST",
 		dataType : "json",
-		async : false,
+		success : function(row){
+			$('#id_ubah').val(id);
+			$('#nama_lengkap_ubah').val(row['nama_user']);
+			$('#username_ubah').val(row['username']);
+		}
+	});
+
+	$('#btn_batal_ubah').click(function(){
+		$('#view_data').show();
+		$('#view_ubah').hide();
+	});
+}
+
+function hapus_data(id){
+	$('#popup_hapus').click();
+
+	$.ajax({
+		url : '<?php echo base_url(); ?>manajemen_user_c/get_user_id',
+		data : {id:id},
+		type : "POST",
+		dataType : "json",
 		success : function(row){
 			$('#id_hapus').val(id);
-			$('#msg').html('Apakah <b>'+row['nama_kategori']+'</b> ini ingin dihapus ?');
+			$('#msg').html('Apakah <b>'+row['username']+'</b> ini ingin dihapus ?');
 		}
 	});
 }
 
-function ubah_data_kategori(id){
-		$('#popup_ubah').css('display','block');
-		$('#popup_ubah').show();
-	
-		$.ajax({
-			url : '<?php echo base_url(); ?>kategori_barang_c/data_kategori_id',
-			data : {id:id},
-			type : "POST",
-			dataType : "json",
-			async : false,
-			success : function(row){
-				$('#id_kategori_modal').val(id);
-				$('#kode_kategori_modal').val(row['kode_kategori']);
-				$('#nama_kategori_modal').val(row['nama_kategori']);
+function get_divisi(){
+	var keyword = "";
+
+	$.ajax({
+		url : '<?php echo base_url(); ?>manajemen_user_c/get_divisi',
+		data : {keyword:keyword},
+		type : "GET",
+		dataType : "json",
+		success : function(result){
+			$tr = '';
+
+			if(result == null || result == ""){
+				$tr = '<tr><td colspan="2">Data Tidak Ada</td></tr>';
+			}else{
+				var no = 0;
+
+				for(var i=0; i<result.length; i++){
+					no++;
+
+					$tr += '<tr style="cursor:pointer;">'+
+								'<td>'+no+'</td>'+
+								'<td>'+result[i].nama_divisi+'</td>'+
+							'</tr>';
+				}
 			}
-		});
+
+			$('#tabel_dep tbody').html($tr);
+		}
+	});
 }
 </script>
-
-<div class="row" id="form_kategori" style="display:none; ">
-	<div class="col-md-12">
-		<!-- BEGIN SAMPLE FORM PORTLET-->
-		<div class="portlet light bordered">
-			<div class="portlet-title">
-				<div class="caption font-green-haze">
-					<i class="icon-settings font-green-haze"></i>
-					<span class="caption-subject bold uppercase"> Form Kategori </span>
-				</div>
-			</div>
-			<div class="portlet-body form">
-				<form role="form" class="form-horizontal" method="post" action="<?php echo $url_simpan; ?>">
-					<div class="form-body">
-						<div class="form-group form-md-line-input">
-							<label class="col-md-2 control-label" for="form_control_1">Nama Lengkap</label>
-							<div class="col-md-8">
-								<input type="text" class="form-control" id="nama_lengkap" name="nama_lengkap" >
-								<div class="form-control-focus">
-								</div>
-							</div>
-						</div>
-						<div class="form-group form-md-line-input">
-							<label class="col-md-2 control-label" for="form_control_1">Username</label>
-							<div class="col-md-8">
-								<input type="text" class="form-control" id="username" name="username" >
-								<div class="form-control-focus">
-								</div>
-							</div>
-						</div>
-						<div class="form-group form-md-line-input">
-							<label class="col-md-2 control-label" for="form_control_1">Password</label>
-							<div class="col-md-8">
-								<input type="text" class="form-control" id="password" name="password" >
-								<div class="form-control-focus">
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group form-md-line-input">
-							<label class="col-md-2 control-label" for="form_control_1">Ulangi Password</label>
-							<div class="col-md-8">
-								<input type="text" class="form-control" id="password_ulang" name="password_ulang" >
-								<div class="form-control-focus">
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="form-actions">
-						<div class="row">
-							<div class="col-md-offset-2 col-md-10">
-								<button type="submit" class="btn blue">Simpan</button>
-								<button type="button" id="batal" class="btn red">Batal Dan Kembali</button>
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-		<!-- END SAMPLE FORM PORTLET-->
-	</div>
-</div>
-
-<button id="tambah_kategori" class="btn green">
-Tambah User <i class="fa fa-plus"></i>
-</button>
-</br>
-</br>
 
 <div class="row" id="view_data">
 	<div class="col-md-12">
@@ -129,120 +106,233 @@ Tambah User <i class="fa fa-plus"></i>
 				</div>		
 			</div>
 			<div class="portlet-body">
+				<button id="btn_tambah" class="btn green" style="margin-bottom: 10px;">
+					Tambah User <i class="fa fa-plus"></i>
+				</button>
 				<table class="table table-striped table-hover table-bordered" id="sample_editable_1">
-				<thead>
-				<tr>
-					<th style="text-align:center;"> No</th>
-					<th style="text-align:center;"> Username</th>
-					<th style="text-align:center;"> Departemen</th>
-					<th style="text-align:center;"> Level</th>
-					<th style="text-align:center;"> Aksi </th>
-				</tr>
-				</thead>
-				<tbody>
-					<?php 
-					$no = 0 ;
-					foreach ($lihat_data as $value) {
-						$no++;
-					?>
-				<tr>
-					<td style="text-align:center; vertical-align:"><?php echo $no; ?></td>
-					<td style="text-align:center; vertical-align:"><?php echo $value->username; ?></td>
-					<td style="text-align:center; vertical-align:"><?php echo $value->nama_departemen; ?></td>
-					<td style="text-align:center; vertical-align:"><?php echo $value->level; ?></td>
-					<td style="text-align:center; vertical-align: middle;">
-						<a class="btn default btn-xs blue" href="<?php echo base_url(); ?>manajemen_user_c/hak_akses/<?php echo $value->id;?>">
-							<i class="fa fa-cog"></i> Kelola Hak Akses 
-						</a>
-						<a class="btn default btn-xs purple" id="ubah" onclick="ubah_data_kategori(<?php echo $value->id?>);">
-							<i class="fa fa-edit"></i> Ubah
-						</a>
-						<a class="btn default btn-xs red" id="hapus" onclick="hapus_kategori(<?php echo $value->id?>);">
-							<i class="fa fa-trash-o"></i> Hapus
-						</a>
-					</td>
-				</tr>
-					<?php 
-						}
-					?>
-				</tbody>
+					<thead>
+						<tr>
+							<th style="text-align:center;"> No</th>
+							<th style="text-align:center;"> Username</th>
+							<th style="text-align:center;"> Departemen</th>
+							<th style="text-align:center;"> Level</th>
+							<th style="text-align:center;"> Aksi </th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php 
+						$no = 0 ;
+						foreach ($lihat_data as $value) {
+							$no++;
+						?>
+						<tr>
+							<td style="text-align:center; vertical-align:"><?php echo $no; ?></td>
+							<td style="text-align:center; vertical-align:"><?php echo $value->username; ?></td>
+							<td style="text-align:center; vertical-align:"><?php echo $value->nama_departemen; ?></td>
+							<td style="text-align:center; vertical-align:"><?php echo $value->level; ?></td>
+							<td style="text-align:center; vertical-align: middle;">
+								<a class="btn default btn-xs blue" href="<?php echo base_url(); ?>manajemen_user_c/hak_akses/<?php echo $value->id;?>">
+									<i class="fa fa-cog"></i> Kelola Hak Akses 
+								</a>
+								<!-- <a class="btn default btn-xs purple" id="ubah" onclick="ubah_data(<?php //echo $value->id?>);">
+									<i class="fa fa-edit"></i> Ubah
+								</a> -->
+								<a class="btn default btn-xs red" id="hapus" onclick="hapus_data(<?php echo $value->id?>);">
+									<i class="fa fa-trash-o"></i> Hapus
+								</a>
+							</td>
+						</tr>
+						<?php 
+							}
+						?>
+					</tbody>
 				</table>
 			</div>
 		</div>
 	</div>
 </div>
 
-<div id="popup_ubah">
-	<div class="window_ubah">
-		<div class="tab-content">
-			<div id="tab_0" class="tab-pane active">
-				<div class="portlet box green">
-					<div class="portlet-title">
-						<div class="caption">
-							<i class="fa fa-pencil"></i>Ubah Kategori
-						</div>
-					</div>
-
-					<div class="portlet-body form">
-						<!-- BEGIN FORM-->
-						<div class="portlet-body form">
-					<form role="form" class="form-horizontal" method="post" action="<?php echo $url_ubah;?>" enctype="multipart/form-data">
-						<div class="form-body">
-							<input type="hidden" name="id_kategori_modal" id="id_kategori_modal">
-
-							<div class="form-group form-md-line-input">
-								<label class="col-md-3 control-label" for="form_control_1">Kode Kategori</label>
-								<div class="col-md-4">
-									<input required type="text" class="form-control" name="kode_kategori_modal" id="kode_kategori_modal" >
-									<div class="form-control-focus">
-									</div>
-								</div>
-							</div>
-
-							<div class="form-group form-md-line-input">
-								<label class="col-md-3 control-label" for="form_control_1">Nama Kategori</label>
-								<div class="col-md-4">
-									<input required type="text" class="form-control" name="nama_kategori_modal" id="nama_kategori_modal" >
-									<div class="form-control-focus">
-									</div>
-								</div>
-							</div>
-
-						<div class="form-actions">
-							<div class="row">
-								<div class="col-md-offset-3 col-md-10">
-									<button type="submit" class="btn blue">Simpan</button>
-									<button type="button" id="batal_ubah" class="btn default">Batal</button>
+<div class="row" id="view_tambah">
+	<div class="col-md-12">
+		<div class="portlet light bordered">
+			<div class="portlet-title">
+				<div class="caption font-green-haze">
+					<i class="icon-settings font-green-haze"></i>
+					<span class="caption-subject bold uppercase"> Form Input User </span>
+				</div>
+			</div>
+			<div class="portlet-body form">
+				<form role="form" class="form-horizontal" method="post" action="<?php echo base_url(); ?>manajemen_user_c/simpan_user">
+					<div class="form-body">
+						<div class="form-group form-md-line-input">
+							<label class="col-md-2 control-label" for="form_control_1">Nama User</label>
+							<div class="col-md-8">
+								<input type="text" class="form-control" id="nama_lengkap" name="nama_lengkap" value="">
+								<div class="form-control-focus">
 								</div>
 							</div>
 						</div>
-				</div>
-			</form>
-		</div>
-										<!-- END FORM-->
+						<div class="form-group form-md-line-input">
+							<label class="col-md-2 control-label" for="form_control_1">Username</label>
+							<div class="col-md-8">
+								<input type="text" class="form-control" id="username" name="username" value="">
+								<div class="form-control-focus">
+								</div>
+							</div>
+						</div>
+						<div class="form-group form-md-line-input">
+							<label class="col-md-2 control-label" for="form_control_1">Password</label>
+							<div class="col-md-8">
+								<input type="password" class="form-control" id="password" name="password" value="">
+								<div class="form-control-focus">
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group form-md-line-input">
+							<label class="col-md-2 control-label" for="form_control_1">Ulangi Password</label>
+							<div class="col-md-8">
+								<input type="password" class="form-control" id="password_ulang" name="password_ulang" value="">
+								<div class="form-control-focus">
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group form-md-line-input">
+							<label class="col-md-2 control-label" for="form_control_1">Level</label>
+							<div class="col-md-8">
+								<select class="form-control" name="level">
+									<option value="direktur">Direktur</option>
+									<option value="manager">Manager</option>
+								</select>
+							</div>
+						</div>
 					</div>
-				</div>
+					<div class="form-actions">
+						<div class="row">
+							<div class="col-md-offset-2 col-md-10">
+								<button type="submit" class="btn blue">Simpan</button>
+								<button type="button" id="btn_batal" class="btn red">Batal</button>
+							</div>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
 </div>
 
-<div id="popup_hapus">
-	<div class="window_hapus">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-body">
-					<button class="bootbox-close-button close" type="button" id="close_hapus">×</button>
-					<div class="bootbox-body" id="msg"></div>
+<div class="row" id="view_ubah">
+	<div class="col-md-12">
+		<div class="portlet light bordered">
+			<div class="portlet-title">
+				<div class="caption font-green-haze">
+					<i class="icon-settings font-green-haze"></i>
+					<span class="caption-subject bold uppercase"> Form Input User </span>
 				</div>
-				<div class="modal-footer">
-					<form action="<?php echo $url_hapus; ?>" method="post">
-						<input type="hidden" name="id_hapus" id="id_hapus" value="">
-						<input type="button" class="btn btn-default" data-bb-handler="cancel" value="Batal" id="batal_hapus">
-						<input type="submit" class="btn btn-primary" data-bb-handler="confirm" value="Hapus" id="hapus">
-					</form>
-				</div>
+			</div>
+			<div class="portlet-body form">
+				<form role="form" class="form-horizontal" method="post" action="">
+					<input type="hidden" name="id_ubah" id="id_ubah" value="">
+					<div class="form-body">
+						<div class="form-group form-md-line-input">
+							<label class="col-md-2 control-label" for="form_control_1">Nama User</label>
+							<div class="col-md-8">
+								<input type="text" class="form-control" id="nama_lengkap_ubah" name="nama_lengkap_ubah" value="">
+								<div class="form-control-focus">
+								</div>
+							</div>
+						</div>
+						<div class="form-group form-md-line-input">
+							<label class="col-md-2 control-label" for="form_control_1">Username</label>
+							<div class="col-md-8">
+								<input type="text" class="form-control" id="username_ubah" name="username_ubah" value="">
+								<div class="form-control-focus">
+								</div>
+							</div>
+						</div>
+						<div class="form-group form-md-line-input">
+							<label class="col-md-2 control-label" for="form_control_1">Password</label>
+							<div class="col-md-8">
+								<input type="text" class="form-control" id="password_ubah" name="password_ubah" value="">
+								<div class="form-control-focus">
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group form-md-line-input">
+							<label class="col-md-2 control-label" for="form_control_1">Ulangi Password</label>
+							<div class="col-md-8">
+								<input type="text" class="form-control" id="password_ulang_ubah" name="password_ulang_ubah" value="">
+								<div class="form-control-focus">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="form-actions">
+						<div class="row">
+							<div class="col-md-offset-2 col-md-10">
+								<button type="submit" class="btn blue">Simpan</button>
+								<button type="button" id="btn_batal_ubah" class="btn red">Batal</button>
+							</div>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
+</div>
+
+<a class="btn default" id="popup_hapus" data-toggle="modal" href="#basic" style="display: none;">View Demo </a>
+<div class="modal fade" id="basic" tabindex="-1" role="basic" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+				<h4 class="modal-title">Konfirmasi Hapus</h4>
+			</div>
+			<div class="modal-body">
+				<p id="msg"></p>
+			</div>
+			<div class="modal-footer">
+				<form action="<?php echo $url_hapus; ?>" method="post">
+					<input type="hidden" name="id_hapus" id="id_hapus" value="">
+					<button type="button" class="btn default" data-dismiss="modal">Tutup</button>
+					<button type="submit" class="btn red">Hapus</button>
+				</form>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+
+<a class="btn default" id="popup_dep" data-toggle="modal" href="#basic2" style="display: none;">View Demo </a>
+<div class="modal fade" id="basic2" tabindex="-1" role="basic" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+				<h4 class="modal-title">Data Divisi</h4>
+			</div>
+			<div class="modal-body">
+				<table class="table table-striped table-hover table-bordered" id="tabel_dep">
+					<thead>
+						<tr>
+							<th style="text-align:center;"> No</th>
+							<th style="text-align:center;"> Nama Divisi</th>
+						</tr>
+					</thead>
+					<tbody>
+						
+					</tbody>
+				</table>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn default" data-dismiss="modal">Tutup</button>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
 </div>
